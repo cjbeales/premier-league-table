bba3f02cf2ac895961e1c90d2b912f9738544dcf
cjbeales <connorj.beales@gmail.com>
2026-05-29 12:14:37 +0100

    Scrolling improved

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@ import { LeagueTable } from "@/components/LeagueTable";
 
 export default function Home() {
   return (
-
     <div className="relative flex flex-1 items-center justify-center">
       <div className=" absolute inset-0 bg-[url('/pl-table-background.jpg')] bg-cover bg-center" />
       <div className="absolute inset-0 bg-black/10" />
       <div className="relative z-10">
-        <main className="flex flex-1 w-full max-w-screen-xl overflow-x-auto flex-col items-center justify-between py-32 sm:items-start">
+        <main className="flex flex-1 w-full max-h-screen max-w-screen-xl overflow-x-auto flex-col items-center justify-between py-0 sm:items-start">
           <LeagueTable />
         </main>
       </div>
