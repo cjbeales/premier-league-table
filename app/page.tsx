47fbaf2168055ddfd7edc1f5b076afdcb5a52d5e
cjbeales <connorj.beales@gmail.com>
2026-05-30 12:15:38 +0100

    Table now mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="relative flex flex-1 items-center justify-center">
       <div className=" absolute inset-0 bg-[url('/pl-bg-blue.jpg')] bg-cover bg-center" />
       <div className="absolute inset-0 bg-black/10" />
-      <div className="relative z-10">
-        <main className="rounded-xl flex flex-1 w-full max-h-[72vh] max-w-screen-xl overflow-x-auto flex-col items-center justify-between py-0 sm:items-start">
+      <div className="relative z-10 p-8 md:p-10 w-full flex justify-center overflow-hidden">
+        <main className="rounded-2xl flex flex-1 w-full max-h-[72vh] max-w-screen-xl overflow-x-auto flex-col items-center justify-between py-0 sm:items-start">
           <LeagueTable />
         </main>
       </div>
