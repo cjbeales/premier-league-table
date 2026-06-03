2c97d0db95642c0000afd7efdf81a92a1d97122a
cjbeales <connorj.beales@gmail.com>
2026-06-03 13:28:39 +0100

    Basic modular scss styling started

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import styles from "@styles/layout.module.scss";
+import classNames from "classnames";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={classNames(geistSans.variable, geistMono.variable, styles.htmlWrapper)}
     >
-      <body className="min-h-full flex flex-col overflow-hidden">{children}</body>
+      <body className={styles.wrapper}>{children}</body>
     </html>
   );
 }
